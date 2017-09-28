82f1980b4664d21727140045f5f9e344d88233fb
Sethuraman Annamalai <sethuramanannamalai@d207-023-192-191.wireless.sfu.ca>
2017-09-28 13:00:37 -0700

    Changes

--- src/house_prices_ensemble.py
+++ src/house_prices_ensemble.py
@@ -9,7 +9,7 @@ import csv
 import matplotlib.pyplot as plt
 
 data = pd.read_csv('../datasets/train.csv')
-submit_data = pd.read_csv('../datasets/test.csv')
+submit_data = pd.read_csv('/Users/sethuramanannamalai/Documents/PyCharm/HousePrices/datasets/test.csv')
 del data['Id']
 submit_data_houseIds = submit_data['Id']
 del submit_data['Id']
@@ -40,14 +40,13 @@ for feature in classi_features:
 ### Data split ###
 data_y = pd.DataFrame(data, columns=['SalePrice'])
 del data['SalePrice']
-train_data_x, test_data_x, train_data_y, test_data_y = train_test_split(data, data_y)
 
 ### Hyper Parameter Tuning - alpha ###
 #Lasso Tuning
 grid_params = {'alpha': [1, 5, 10, 0.1, 0.01, 0.001, 0.0001]}
 lasso_model = Lasso(normalize=True)
 grid_model = GridSearchCV(lasso_model, grid_params, scoring='neg_mean_squared_error', cv=10, verbose=1, n_jobs=-1)
-grid_model.fit(train_data_x, train_data_y)
+grid_model.fit(data, data_y)
 tuned_alpha = grid_model.best_params_.get('alpha')
 #XGB Tuning
 # grid_params = {'learning_rate': [0.001, 0.0001],
@@ -64,17 +63,15 @@ tuned_alpha = grid_model.best_params_.get('alpha')
 
 ### Training ###
 lasso_model = Lasso(alpha=tuned_alpha, normalize=True)
-lasso_model.fit(train_data_x, train_data_y)
+lasso_model.fit(data, data_y)
 xgb_model = XGBRegressor(n_estimators=300, reg_lambda=0.001, reg_alpha=0.01, learning_rate=0.1, booster='gbtree')
-xgb_model.fit(train_data_x, train_data_y)
+xgb_model.fit(data, data_y)
 
 ### Performance ###
 print 'Lasso Mode :'
-print 'Cross validation score = ' + str(np.mean(cross_val_score(lasso_model, train_data_x, train_data_y, cv=10)))
-print 'R score = ' + str(lasso_model.score(train_data_x, train_data_y))
+print 'Cross validation score = ' + str(np.mean(cross_val_score(lasso_model, data, data_y, cv=10)))
 print 'XGBoost Regressor :'
-print 'Cross validation score = ' + str(np.mean(cross_val_score(xgb_model, train_data_x, train_data_y, cv=10)))
-print 'R score = ' + str(xgb_model.score(train_data_x, train_data_y))
+print 'Cross validation score = ' + str(np.mean(cross_val_score(xgb_model, data, data_y, cv=10)))
 
 predictions_lasso = lasso_model.predict(submit_data)
 predictions_xgb = xgb_model.predict(submit_data)
