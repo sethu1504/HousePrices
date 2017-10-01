327080a796d537a52cbf013e65abbd3eaa9b8348
Sethuraman Annamalai <sethuramanannamalai@Sethuramans-MacBook-Pro.local>
2017-09-30 19:07:06 -0700

    Comments added

--- src/house_prices_ensemble.py
+++ src/house_prices_ensemble.py
@@ -13,11 +13,11 @@ submit_data = pd.read_csv('/Users/sethuramanannamalai/Documents/PyCharm/HousePri
 del data['Id']
 submit_data_houseIds = submit_data['Id']
 del submit_data['Id']
-######  FEATURE ENGINEERING ########
+# FEATURE ENGINEERING
 
-### Numeric Features #####
+# Numeric Features
 numeric_features = list(data.dtypes[data.dtypes != 'object'].index)
-numeric_features.remove('MSSubClass')  #This variable is a classification feature
+numeric_features.remove('MSSubClass')  # This variable is a classification feature
 for feature in numeric_features:
     data[feature].fillna(np.mean(data[feature]), inplace=True)
     data[feature] = np.log1p(data[feature])
@@ -25,7 +25,7 @@ for feature in numeric_features:
         submit_data[feature].fillna(np.mean(submit_data[feature]), inplace=True)
         submit_data[feature] = np.log1p(submit_data[feature])
 
-### Classification features - introduce dummy variables ####
+# Classification features - introduce dummy variables
 classi_features = list(data.dtypes[data.dtypes == 'object'].index)
 classi_features.append('MSSubClass')
 for feature in classi_features:
@@ -37,18 +37,18 @@ for feature in classi_features:
         submit_data[str(feature + '_' + str(elem))] = pd.Series(submit_data[feature] == elem, dtype=int)
     del submit_data[feature]
 
-### Data split ###
+# Data split
 data_y = pd.DataFrame(data, columns=['SalePrice'])
 del data['SalePrice']
 
-### Hyper Parameter Tuning - alpha ###
-#Lasso Tuning
+# Hyper Parameter Tuning - alpha
+# Lasso Tuning
 grid_params = {'alpha': [1, 5, 10, 0.1, 0.01, 0.001, 0.0001]}
 lasso_model = Lasso(normalize=True)
 grid_model = GridSearchCV(lasso_model, grid_params, scoring='neg_mean_squared_error', cv=10, verbose=1, n_jobs=-1)
 grid_model.fit(data, data_y)
 tuned_alpha = grid_model.best_params_.get('alpha')
-#XGB Tuning
+# XGB Tuning
 # grid_params = {'learning_rate': [0.001, 0.0001],
 #           'booster': ['gblinear', 'gbtree'],
 #           'n_estimators': [200, 300],
@@ -61,13 +61,13 @@ tuned_alpha = grid_model.best_params_.get('alpha')
 # print grid_model.best_params_
 # Result - {'n_estimators': 300, 'reg_lambda': 0.001, 'learning_rate': 0.001, 'reg_alpha': 0.01, 'booster': 'gbtree'}
 
-### Training ###
+# Training
 lasso_model = Lasso(alpha=tuned_alpha, normalize=True)
 lasso_model.fit(data, data_y)
 xgb_model = XGBRegressor(n_estimators=300, reg_lambda=0.001, reg_alpha=0.01, learning_rate=0.1, booster='gbtree')
 xgb_model.fit(data, data_y)
 
-### Performance ###
+# Performance
 print 'Lasso Mode :'
 print 'Cross validation score = ' + str(np.mean(cross_val_score(lasso_model, data, data_y, cv=10)))
 print 'XGBoost Regressor :'
